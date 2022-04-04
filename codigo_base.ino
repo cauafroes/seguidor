004b884494dcb5b305d726eade6e4cba3f68dc5a
Cauã Fróes <67842825+cauafroes@users.noreply.github.com>
2022-04-04 19:39:49 -0300

    Add files via upload

---
+++ codigo_base.ino
@@ -0,0 +1,131 @@
+#define ir1 4
+#define ir2 11
+#define ir3 12
+#define pinMotorEn1 5 //en = enable
+#define pinMotorEn2 6 //sao usadas para controlar a velocidade
+#define pinMotorA1 7
+#define pinMotorA2 8
+#define pinMotorB1 9
+#define pinMotorB2 10
+
+int sensor1, sensor2, sensor3 = 0;
+int velMotorA, velMotorB = 0;
+int somaConst = 0;
+char direcao = "r";
+
+void lerSensores(){
+  sensor1 = digitalRead(ir1); 
+  sensor2 = digitalRead(ir2); 
+  sensor3 = digitalRead(ir3);
+
+  /*      debugging
+  Serial.print("Sensor 1 = ");
+  Serial.print(valorSensor1);
+  Serial.print("    ");
+  Serial.print("Sensor 2 = ");
+  Serial.print(valorSensor2);
+  Serial.print("    ");
+  Serial.print("Sensor 3 = ");
+  Serial.print(valorSensor3);
+  */
+
+}
+
+void mover(char direcao){
+  switch (direcao){
+    case 'f':
+      digitalWrite(pinMotorA1, HIGH);
+      digitalWrite(pinMotorA2, LOW);
+      digitalWrite(pinMotorB1, HIGH);
+      digitalWrite(pinMotorB2, LOW);
+      break;
+    case 'd':
+      digitalWrite(pinMotorA1, HIGH);
+      digitalWrite(pinMotorA2, LOW);
+      digitalWrite(pinMotorB1, LOW);
+      digitalWrite(pinMotorB2, HIGH);
+      break;
+    case 'e':
+      digitalWrite(pinMotorA1, LOW);
+      digitalWrite(pinMotorA2, HIGH);
+      digitalWrite(pinMotorB1, HIGH);
+      digitalWrite(pinMotorB2, LOW);
+      break;
+    case 't':
+      digitalWrite(pinMotorA1, LOW);
+      digitalWrite(pinMotorA2, HIGH);
+      digitalWrite(pinMotorB1, LOW);
+      digitalWrite(pinMotorB2, HIGH);
+      break;
+    case 'r':
+      digitalWrite(pinMotorA1, LOW);
+      digitalWrite(pinMotorA2, LOW);
+      digitalWrite(pinMotorB1, LOW);
+      digitalWrite(pinMotorB2, LOW);
+      break;
+  }
+}
+
+void movimentos(){
+  if(sensor2 == 0){
+    if(sensor1 == 1 && sensor3 == 1){
+      mover('f');
+    }
+    else if(sensor1 == 0 && sensor3 == 1){
+      //curva 90 graus esquerda
+      mover('e');
+      delay(30);
+      mover("f");
+      delay(30);
+      mover("r");
+    }
+    else if(sensor1 == 1 && sensor3 == 0){
+      //curva 90 graus direita
+      mover("d");
+      delay(30);
+      mover("f");
+      delay(30);
+      mover("r");
+    }
+    else if(sensor1 == 0 && sensor3 == 0){
+      //array de sensores paralelo a linha
+      //manda o robo seguir reto
+      //tem mais trabalho a ser feito aqui
+      mover("f");
+      delay(50);
+    }
+  }
+  else if(sensor1 == 0){
+    if(sensor2 == 1 && sensor3 == 1){
+      mover('e');
+    }
+  }
+  else if(sensor3 == 0){
+    if(sensor1 == 1 && sensor2 == 1){
+      mover('d');
+    }
+  }
+}
+
+void setup(){
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(ir3, INPUT);
+  pinMode(pinMotorA1, OUTPUT);
+  pinMode(pinMotorA2, OUTPUT);
+  pinMode(pinMotorB1, OUTPUT);
+  pinMode(pinMotorB2, OUTPUT);
+  pinMode(pinMotorEn1, OUTPUT);
+  pinMode(pinMotorEn2, OUTPUT);
+
+  //como nao estamos usando pwm, botei os motores sempre na máxima
+  digitalWrite(pinMotorEn1, HIGH);
+  digitalWrite(pinMotorEn2, HIGH);
+
+  Serial.begin(9600);
+}
+
+void loop(){
+  lerSensores();
+  movimentos();
+}
